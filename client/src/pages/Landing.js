0f895c60fcaa9d5f1cf431feaba9e8a060af656b
HoleHorse <tamirlankulzhanov@gmail.com>
2023-03-02 21:18:43 +0600

    rm old html

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,7 +4,7 @@ function Landing() {
   return (
     <div className="landing-group">
       <img src={logo} alt="Logo" width={356}></img>
-      <h1>Set, keep track and acheve your goals with us!</h1>
+      <h1 style={{fontSize: 4+"vh"}}>Set, keep track and acheve your goals with us!</h1>
       <a href="/login" className="btn btn-dark">
         Get started
       </a>
