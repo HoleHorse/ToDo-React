681b63317db648af464c58caae608551d655d4eb
HoleHorse <tamirlankulzhanov@gmail.com>
2023-03-10 16:48:57 +0600

    admin back

--- server/handlers/admin.go
+++ server/handlers/admin.go
@@ -2,13 +2,12 @@ package handlers
 
 import (
 	"ToDo/database"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func getUsers(c *gin.Context) []User {
+func GetUsers(c *gin.Context) {
 	users := database.Client.Database("project").Collection("users")
 	filter := bson.D{}
 	cursor, _ := users.Find(c, filter)
@@ -21,13 +20,17 @@ func getUsers(c *gin.Context) []User {
 		}
 		results = append(results, result)
 	}
-	return results
+	writeResult(c.Writer, results)
 }
 
 func DeleteUser(c *gin.Context) {
 	users := database.Client.Database("project").Collection("users")
 	user := c.Param("user")
 	filter := bson.D{{Key: "username", Value: user}}
-	users.DeleteOne(c, filter)
-	c.Redirect(http.StatusSeeOther, "/admin")
+	_, err := users.DeleteOne(c, filter)
+	if err != nil {
+		writeResult(c.Writer, err.Error())
+		return
+	}
+	writeResult(c.Writer, "success")
 }
--- server/main.go
+++ server/main.go
@@ -8,13 +8,18 @@ import (
 
 func main() {
 	router := gin.Default()
+	// Auth
 	router.POST("/register", handlers.Register)
 	router.POST("/login", handlers.Login)
+	router.POST("/validate/:username", handlers.CheckUsername)
+	router.POST("/validate/", handlers.EmptyUsername)
+	// ToDo
 	router.GET("/todo/:id", handlers.GetToDo)
 	router.POST("/edit/:id", handlers.EditToDo)
 	router.POST("/delete/:id", handlers.DeleteToDo)
 	router.POST("/add", handlers.AddToDo)
-	router.POST("/validate/:username", handlers.CheckUsername)
-	router.POST("/validate/", handlers.EmptyUsername)
+	// Admin
+	router.GET("/admin", handlers.GetUsers)
+	router.POST("/admin/delete/:user", handlers.DeleteUser)
 	router.Run("localhost:4000")
 }
